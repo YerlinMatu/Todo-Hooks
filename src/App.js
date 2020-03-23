48fb8c42b0b31e7fa28aab839840f48bac7ceeae
Yerlin Matu <maicolmatu27@gmail.com>
2020-03-23 09:19:11 -0500

    Feat: added node sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 // @components
 import { Typography } from '@material-ui/core';
@@ -11,13 +11,12 @@ const App = () => {
   const { todos, addTodo, deleteTodo } = useTodoState([]);
 
   return (
-    <div className="App">
+    <div className="container">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
       <TodoForm saveTodo={(todoText) => {
         const trimmedText = todoText.trim();
-
         trimmedText && addTodo(trimmedText);
       }}/>
       <TodoList todos={todos} deleteTodo={deleteTodo} />
